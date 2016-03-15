26bd7089140b7627d15a50599c16646b803be015
BRITTANY ANDREWS <brittanyandrewsnyc@gmail.com>
2016-03-15 14:33:36 -0400

    final commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,26 @@
-// Javascript Entry Point
+import $ from 'jquery';
+
+var gh_url = "https://api.github.com/users/jisaacks/orgs";
+
+
+
+$.ajax(gh_url).then(function(data){
+	var visible=data.forEach(function({login, avatar_url}){
+		$('.wrapper').append(`<div class="containers">
+			<img class="avatar" src="${avatar_url}">
+			<span>${login}</span>
+			</div>`);
+	})
+})
+
+
+
+
+
+
+
+
+
+
+
+
